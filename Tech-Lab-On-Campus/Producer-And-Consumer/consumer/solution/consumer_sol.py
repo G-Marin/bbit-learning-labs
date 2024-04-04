54bf7882fbb29dd6154d8499f0b097deed8d84a3
Gabriel <gm2594@princeton.edu>
2024-04-04 19:49:46 -0400

    typo

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -48,7 +48,7 @@ class mqConsumer(mqConsumerInterface):
         # Acknowledge message
         channel.basic_ack(method_frame.delivery_tag, False)
         #Print message (The message is contained in the body parameter variable)
-        print("The message is contained in the body parameter variable")
+        print(body)
 
     def startConsuming(self) -> None:
         # Print " [*] Waiting for messages. To exit press CTRL+C"
